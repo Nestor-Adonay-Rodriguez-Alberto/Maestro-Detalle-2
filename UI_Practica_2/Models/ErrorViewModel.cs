a5f150c5d4469634f08199535f7ab62d82f8d85c
Nestor.Adonay.Rodriguez.Alberto@gmail.com <nestor.adonay.rodriguez.alberto@gmail.com>
2024-06-29 22:32:02 -0600

    Agregar archivos de proyecto.

---
+++ UI_Practica_2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UI_Practica_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
